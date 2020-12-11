ff886f77fa6cb73e135edc992ae3c918cb20d923
Sebastian-C21 <sebastian981211@gmail.com>
2020-12-10 20:36:47 -0500

    Arreglando la importada

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from db.db import UserInDB, get_user, update_password, UserOut, passUpdate, passOut
+from db.user_db import UserInDB, get_user, update_pass_, update_password, UserOut, passUpdate, passOut
 
 import datetime
 from fastapi import FastAPI
@@ -16,9 +16,4 @@ async def get_data(username: str):
 
 @api.put("/user/updateUser/")
 async def update_pass(name: passUpdate):
-    user_in_db = get_user(name.nombre)
-    if user_in_db == None:
-        raise HTTPException(status_code=404,detail="El usuario no existe")
-    update_password(user_in_db.nombre,user_in_db.password)
-    pass_out = passOut(**user_in_db.dict())
-    return pass_out
+    update_pass_(name)
